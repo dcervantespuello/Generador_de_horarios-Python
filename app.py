0d3c81c7a4971a7c0c77f0c3a3e3c86060a8bd3b
IngDeivis <deivis123211@gmail.com>
2019-10-01 15:12:51 -0500

    Reducción del código de app.py con archivo para funciones

--- app.py
+++ app.py
@@ -1,38 +1,6 @@
-# Se importa Pandas y Numpy
 import pandas as pd
-from py.cursos import arreglo_cursos
+from py.funciones import *
 
-# Se lee el dataframe
 df = pd.read_csv('csv/dataframe.csv')
-
-# Se quitan las filas con valor 'NULO' en columna 'Codigo_docente'
-df = df[df.Codigo_docente != 'NULO']
-
-# Quitar filas con valor 0 en columnas 'Capacidad', 'Disponibles' y 'Ocupados'
-df = df[df[['Capacidad', 'Disponibles', 'Ocupados']].any(axis='columns')]
-
-# Cambiar valores de columna 'Disponibles' a 0
-df.Disponibles[df.Capacidad == 0] = 0
-
-# Copiar valores de columna 'Ocupados' en columna 'Capacidad'
-df.Capacidad[df.Capacidad == 0] = df.Ocupados
-
-# Conteo de cuantos cursos hay en total
-cursos = set(df.Nombre_asignatura)
-print('Hay {} cursos en el segundo semestre del 2019\n'.format(len(cursos)))
-
-# Organizar los indices del dataframe
-df = df.set_index(['Nombre_asignatura'])
-df = df.sort_index()
-
-print('SECCIONES POR CURSO:\n')
-semestre = 1
-
-# cursos = set(df.index.get_level_values(0))
-# Numero de secciones por curso
-for grupo in arreglo_cursos:
-    print('Semestre {}:'.format(semestre))
-    for curso in grupo:
-        print('{}: {}'.format(curso, len(df.loc[curso, 'Nrc'])))
-    print("")
-    semestre += 1
+df = limpiar_dataframe(df)
+total_cursos(df)
