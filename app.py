17a98b70ab7d969e0de0f604c4c3de51c06dea42
IngDeivis <deivis123211@gmail.com>
2019-10-01 14:27:17 -0500

    Se cuenta el total de cursos y numero de filas de los cursos de sistemas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Se importa Pandas y Numpy
 import pandas as pd
+from py.cursos import arreglo_cursos
 
 # Se lee el dataframe
 df = pd.read_csv('csv/dataframe.csv')
@@ -18,4 +19,20 @@ df.Capacidad[df.Capacidad == 0] = df.Ocupados
 
 # Conteo de cuantos cursos hay en total
 cursos = set(df.Nombre_asignatura)
-print('Hay {} cursos en el segundo semestre del 2019'.format(len(cursos)))
+print('Hay {} cursos en el segundo semestre del 2019\n'.format(len(cursos)))
+
+# Organizar los indices del dataframe
+df = df.set_index(['Nombre_asignatura'])
+df = df.sort_index()
+
+print('SECCIONES POR CURSO:\n')
+semestre = 1
+
+# cursos = set(df.index.get_level_values(0))
+# Numero de secciones por curso
+for grupo in arreglo_cursos:
+    print('Semestre {}:'.format(semestre))
+    for curso in grupo:
+        print('{}: {}'.format(curso, len(df.loc[curso, 'Nrc'])))
+    print("")
+    semestre += 1
